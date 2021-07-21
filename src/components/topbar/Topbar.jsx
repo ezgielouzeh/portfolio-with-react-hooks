ca22e29f0b285d9fcdc3ff21e7d43ed37ef96153
Ezgi Sirip <ezgisirip@gmail.com>
2021-07-21 14:19:52 -0400

    Adds Material UI elements

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,8 +1,23 @@
 import "./topbar.scss"
+import {Person, Mail} from "@material-ui/icons"
 
 export default function Topbar() {
   return (
     <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">Ezgi Elouzeh</a>
+          <div className="itemContainer">
+            <Person className="icon"/>
+            <span>(201)981-4289</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon"/>
+            <span>ezgisirip@gmail.com</span>
+          </div>
+        </div>
+        <div className="right"></div>
+      </div>
 
     </div>
   )
