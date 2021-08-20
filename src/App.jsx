207bafd38c39055e78972965d2e0bd826cc7723b
Ezgi Sirip <ezgisirip@gmail.com>
2021-08-20 16:10:36 -0400

    adds menu bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import Topbar from "./components/topbar/Topbar"
 import Intro from "./components/intro/Intro"
+import Menu from "./components/menu/Menu"
 import Portfolio from "./components/portfolio/Portfolio"
 import Works from "./components/works/Works"
 import Testimonials from "./components/testimonials/Testimonials"
 import Contact from "./components/contact/Contact"
 import "./app.scss"
 import {useState} from "react"
+import { MenuBook } from "@material-ui/icons"
 
 function App() {
   const [menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="app">
     <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+    <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
     <div className="sections">
       <Intro />
       <Portfolio />
