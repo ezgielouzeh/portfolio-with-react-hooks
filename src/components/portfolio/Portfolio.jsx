4ee4f6a95f630fc41bd923cba209885761c7b51a
Ezgi Sirip <ezgisirip@gmail.com>
2021-08-24 18:40:53 -0400

    portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,36 @@ import "./portfolio.scss"
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-
+      <h1>Portfolio</h1>
+      <ul>
+        <li>Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="https://images.freeimages.com/images/small-previews/33a/web-design-1243586.jpg" alt=""/>
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="https://images.freeimages.com/images/small-previews/33a/web-design-1243586.jpg" alt=""/>
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="https://images.freeimages.com/images/small-previews/33a/web-design-1243586.jpg" alt=""/>
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="https://images.freeimages.com/images/small-previews/33a/web-design-1243586.jpg" alt=""/>
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="https://images.freeimages.com/images/small-previews/33a/web-design-1243586.jpg" alt=""/>
+          <h3>Banking App</h3>
+        </div>
+      </div>
     </div>
   )
 }
