e72b425a7cc517b32922293fbf98d1363f8e8fdb
Ezgi Sirip <ezgisirip@gmail.com>
2021-08-24 17:55:09 -0400

    adds animation for intro text

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,21 @@
 import "./intro.scss"
+import { init } from 'ityped'
+import {useEffect, useRef} from "react"
 
 export default function Intro() {
+
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      // backDelay: 1500,
+      // backSpeed: 60,
+      showCursor: true,
+      strings: ['Software Developer', 'Web Designer', 'Educational Technologist' ] })
+  }, [])
+
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -12,7 +27,7 @@ export default function Intro() {
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
           <h1>Ezgi Elouzeh</h1>
-          <h3>Software <span>Developer</span></h3>
+          <h3>Freelance <span ref={textRef}></span></h3>
         </div>
         <a href="#portfolio">
           <img src="assets/down.png" alt=""/>
