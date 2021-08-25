aeb1a1fa1b8d09a6dfd8b31c2760dfc5b4de015c
Ezgi Sirip <ezgisirip@gmail.com>
2021-08-25 17:08:43 -0400

    dynamic data with useEffect hook

---
+++ src/data.js
@@ -0,0 +1,89 @@
+export const featuredPortfolio= [
+  {
+  id:1,
+  title: "Social Media App",
+  img: "***link***"
+  },
+  {
+    id:2,
+    title: "Rampa UI Design",
+    img: "***link***"
+    },
+    {
+      id:3,
+      title: "E-commerce Web Design",
+      img: "***link***"
+      },
+];
+
+export const webPortfolio= [
+  {
+  id:1,
+  title: "Web Media App",
+  img: "***link***"
+  },
+  {
+    id:2,
+    title: "Web Rampa UI Design",
+    img: "***link***"
+    },
+    {
+      id:3,
+      title: "Web E-commerce Web Design",
+      img: "***link***"
+      },
+];
+
+export const mobilePortfolio= [
+  {
+  id:1,
+  title: "Mobile Social Media App",
+  img: "***link***"
+  },
+  {
+    id:2,
+    title: "Mobile Rampa UI Design",
+    img: "***link***"
+    },
+    {
+      id:3,
+      title: "Mobile E-commerce Web Design",
+      img: "***link***"
+      },
+];
+
+export const designPortfolio= [
+  {
+  id:1,
+  title: "Social Media App",
+  img: "***link***"
+  },
+  {
+    id:2,
+    title: "Rampa UI Design",
+    img: "***link***"
+    },
+    {
+      id:3,
+      title: "E-commerce Web Design",
+      img: "***link***"
+      },
+];
+
+export const contentPortfolio= [
+  {
+  id:1,
+  title: "Content Social Media App",
+  img: "***link***"
+  },
+  {
+    id:2,
+    title: "Content Rampa UI Design",
+    img: "***link***"
+    },
+    {
+      id:3,
+      title: "Content E-commerce Web Design",
+      img: "***link***"
+      },
+];
